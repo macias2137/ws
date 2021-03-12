e1a65217c39fd79e56e1f9425e880284757153b2
Maciek <moroz128@gmail.com>
2021-03-12 17:48:42 +0100

    set up sass with yarn

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,19 @@
 import * as React from "react"
-import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = props => (
+  <header>
+    <div>
+      <StaticImage
+        height="4rem"
+        className="logo"
+        src="../images/logo-pis.png"
+        alt="pischuj"
+      />
     </div>
+    <div className="menu-right"></div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
